aa79d3f8c279ed1c2f13023876e91598adea246d
RayCheung <49422705+iRayCheung@users.noreply.github.com>
2023-08-16 16:22:21 +0800

    Update GetSignApplication.java

--- src/main/java/mydlq/club/GetSignApplication.java
+++ src/main/java/mydlq/club/GetSignApplication.java
@@ -22,7 +22,7 @@ public class GetSignApplication {
         String port = env.getProperty("server.port");
         String path = StringUtils.isEmpty(env.getProperty("server.servlet.context-path"))? "" : env.getProperty("server.servlet.context-path").trim();
         String path1 = StringUtils.isEmpty(env.getProperty("server.servlet.context-path"))? "" : env.getProperty("server.servlet.context-path").trim();
-        
+        String c = "test";
         log.info("\n----------------------------------------------------------\n\t" +
                 "IovApplication is running! Access URLs:\n\t" +
                 "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
